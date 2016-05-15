23534b3d6eb06faca0139569200b57733294b67a
Vitaly Svyatyuk <vetal_sv@bk.ru>
2016-05-15 11:39:30 +0300

    Next one

--- note/views.py
+++ note/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-
+    name = "Jefrey"
+    text = "Sometext"
     context = {
         'name': name,
         'text': text,
     }
     return render(request, 'home.html', context)
+
+def note(request):
+    name = "Jefrey"
+    text = "Sometext"
+    context = {
+        'name': name,
+        'text': text,
+    }
+    return render(request, 'note.html', context)
