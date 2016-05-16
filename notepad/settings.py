c9266e355038629a0f0e0d7cca0228c72f8d7cd8
Vitaly <goodgame1945@gmail.com>
2016-05-16 18:19:49 +0300

    Add

--- notepad/settings.py
+++ notepad/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'note',
+
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -122,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda x: True
+}
