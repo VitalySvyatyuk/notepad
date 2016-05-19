36e3f16075ceb68e011122d1943c82fd958a753b
Vitaly <goodgame1945@gmail.com>
2016-05-19 18:28:10 +0300

    procfile

--- notepad/settings.py
+++ notepad/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticStorage'
+
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda x: True
 }
